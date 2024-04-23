1e863e0a24a9e57ce6417acdf2c4ac240d7681ca
Kondziow <konradwojtanowski02@gmail.com>
2024-04-23 19:05:11 +0200

    testGetAllCurrencyRequests test added

---
+++ currency-app-java/src/test/java/wojtanowski/konrad/currencyapp/currencyRequest/controller/CurrencyRequestControllerIT.java
@@ -0,0 +1,7 @@
+package wojtanowski.konrad.currencyapp.currencyRequest.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyRequestControllerIT {
+
+}
---
+++ currency-app-java/src/test/java/wojtanowski/konrad/currencyapp/currencyRequest/controller/CurrencyRequestControllerTest.java
@@ -0,0 +1,55 @@
+package wojtanowski.konrad.currencyapp.currencyRequest.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import wojtanowski.konrad.currencyapp.currencyRequest.model.dto.GetCurrencyRequestDTO;
+import wojtanowski.konrad.currencyapp.currencyRequest.model.dto.GetCurrencyRequestsDTO;
+import wojtanowski.konrad.currencyapp.currencyRequest.service.api.CurrencyRequestService;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(CurrencyRequestController.class)
+class CurrencyRequestControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    CurrencyRequestService currencyRequestService;
+
+    @Test
+    void testGetAllCurrencyRequests() throws Exception {
+        given(currencyRequestService.getAllCurrencyRequests())
+                .willReturn(new GetCurrencyRequestsDTO(
+                        Arrays.asList(getGetCurrencyRequestDTO1(), getGetCurrencyRequestDTO2())
+                ));
+
+        mockMvc.perform(get(CurrencyRequestController.CURRENCIES_GET_PATH))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.currencyRequests").isArray())
+                .andExpect(jsonPath("$.currencyRequests.length()", is(2)));
+
+
+    }
+
+    GetCurrencyRequestDTO getGetCurrencyRequestDTO1() {
+        return new GetCurrencyRequestDTO("EUR", "JAN KOWALSKI", new Timestamp(System.currentTimeMillis()), 4.6F);
+    }
+
+    GetCurrencyRequestDTO getGetCurrencyRequestDTO2() {
+        return new GetCurrencyRequestDTO("EUR", "KONRAD WISNIEWSKI", new Timestamp(System.currentTimeMillis()), 4.5F);
+    }
+}
