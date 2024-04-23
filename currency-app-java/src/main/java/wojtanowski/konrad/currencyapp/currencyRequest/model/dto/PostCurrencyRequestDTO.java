098cc917eaf28a0e818d827e2836bd1170dbc4cd
Kondziow <konradwojtanowski02@gmail.com>
2024-04-23 15:35:06 +0200

    PostCurrencyRequestDTO Validation added

--- currency-app-java/src/main/java/wojtanowski/konrad/currencyapp/currencyRequest/model/dto/PostCurrencyRequestDTO.java
+++ currency-app-java/src/main/java/wojtanowski/konrad/currencyapp/currencyRequest/model/dto/PostCurrencyRequestDTO.java
@@ -1,7 +1,18 @@
 package wojtanowski.konrad.currencyapp.currencyRequest.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record PostCurrencyRequestDTO(
+        @NotNull
+        @NotBlank
+        @Size(min = 3, max = 3)
         String currencyName,
+
+        @NotNull
+        @NotBlank
+        @Size(max = 100)
         String requesterName
 ) {
 }
