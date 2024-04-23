b598002774375c69f2e24afe5b24c65a7c43d7cd
Kondziow <konradwojtanowski02@gmail.com>
2024-04-23 20:11:30 +0200

    Saving currencyRequest into database

--- currency-app-java/src/main/java/wojtanowski/konrad/currencyapp/currencyRequest/controller/CurrencyRequestController.java
+++ currency-app-java/src/main/java/wojtanowski/konrad/currencyapp/currencyRequest/controller/CurrencyRequestController.java
@@ -1,6 +1,5 @@
 package wojtanowski.konrad.currencyapp.currencyRequest.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
