27da340b5b34750b601d15e63e77fada55ce5105
Kondziow <konradwojtanowski02@gmail.com>
2024-05-08 14:41:24 +0200

    DatePipe added

--- currency-app-angular/src/app/currency/currency-item/currency-item.component.ts
+++ currency-app-angular/src/app/currency/currency-item/currency-item.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {CurrencyRecordModel} from "../models/currencyRecord.model";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-currency-item',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './currency-item.component.html',
   styleUrl: './currency-item.component.css'
 })
